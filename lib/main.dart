44f4d8b054b4d5883be65bb0df68ffa32b78646b
Mofolasayo Osikoya <osikoyamofolasayo@gmail.com>
2024-08-07 10:03:21 +0100

    initialize widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeRight,
   ]);
