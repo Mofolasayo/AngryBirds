d34c9dd9c53f7418eb14bf07cbce6576d00590d9
fidelity2851 <ukwuanifidelis2851@gmail.com>
2024-08-07 15:04:21 +0100

    Done with the Pause, Game Won, Game Lost Screens

--- lib/screens/overlays/pause_overlay_screen.dart
+++ lib/screens/overlays/pause_overlay_screen.dart
@@ -6,134 +6,161 @@ class PauseOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: MediaQuery.sizeOf(context).width / 2,
-        height: MediaQuery.sizeOf(context).height / 1.6,
-        padding: const EdgeInsets.all(10),
-        clipBehavior: Clip.hardEdge,
-        decoration: BoxDecoration(
-          color: const Color(0xFF035DA7),
-          border: Border.all(
-            color: Colors.white,
-            width: 5,
-          ),
-          borderRadius: const BorderRadius.all(Radius.circular(30)),
-        ),
-        child: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(
-            children: [
-              const Center(
-                child: Text(
-                  'Paused',
-                  style: TextStyle(
-                    color: Colors.white,
-                    height: 1,
-                    fontSize: 40,
-                    fontWeight: FontWeight.w800,
-                  ),
+    return Container(
+      color: Color.fromARGB(132, 20, 20, 20),
+      child: Center(
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.sizeOf(context).width / 2,
+              height: MediaQuery.sizeOf(context).height / 1.6,
+              padding: EdgeInsets.all(10),
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                color: Color(0xFF035DA7),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 5,
                 ),
+                borderRadius: BorderRadius.all(Radius.circular(30)),
               ),
-              const SizedBox(height: 10),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF047EE2),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    children: [
-                      const Center(
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: Column(
+                  children: [
+                    const Center(
+                      child: Text(
+                        'Paused',
+                        style: TextStyle(
+                          color: Colors.white,
+                          height: 1,
+                          fontSize: 40,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF047EE2),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
                         child: Column(
                           children: [
-                            Text(
-                              'Current Score',
-                              style: TextStyle(
-                                color: Colors.white,
-                                height: 1,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w800,
+                            Center(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Current Score',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      height: 1,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    '10,000',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      height: 1,
+                                      fontSize: 50,
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                            SizedBox(height: 5),
-                            Text(
-                              '10,000',
-                              style: TextStyle(
-                                color: Colors.white,
-                                height: 1,
-                                fontSize: 50,
-                                fontWeight: FontWeight.w800,
-                              ),
+                            Spacer(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(15),
+                                  width: 80,
+                                  height: 80,
+                                  decoration: BoxDecoration(
+                                      color: Color(0xFF035DA7),
+                                      border: Border.all(
+                                        color: Colors.white,
+                                        width: 3,
+                                      ),
+                                      borderRadius: BorderRadius.circular(100)),
+                                  child: SvgPicture.asset(
+                                    color: Color(0xFFFFB159),
+                                    'assets/images/menu_btn.svg',
+                                  ),
+                                ),
+                                SizedBox(width: 30),
+                                Container(
+                                  padding: EdgeInsets.all(15),
+                                  width: 80,
+                                  height: 80,
+                                  decoration: BoxDecoration(
+                                      color: Color(0xFF035DA7),
+                                      border: Border.all(
+                                        color: Colors.white,
+                                        width: 3,
+                                      ),
+                                      borderRadius: BorderRadius.circular(100)),
+                                  child: SvgPicture.asset(
+                                    color: Color(0xFFFFB159),
+                                    'assets/images/restart_btn.svg',
+                                  ),
+                                ),
+                                SizedBox(width: 30),
+                                Container(
+                                  padding: EdgeInsets.all(15),
+                                  width: 80,
+                                  height: 80,
+                                  decoration: BoxDecoration(
+                                      color: Color(0xFF035DA7),
+                                      border: Border.all(
+                                        color: Colors.white,
+                                        width: 3,
+                                      ),
+                                      borderRadius: BorderRadius.circular(100)),
+                                  child: SvgPicture.asset(
+                                    color: Color(0xFFFFB159),
+                                    'assets/images/next_btn.svg',
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
                       ),
-                      const Spacer(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(15),
-                            width: 80,
-                            height: 80,
-                            decoration: BoxDecoration(
-                                color: const Color(0xFF035DA7),
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 3,
-                                ),
-                                borderRadius: BorderRadius.circular(100)),
-                            child: SvgPicture.asset(
-                              color: const Color(0xFFFFB159),
-                              'assets/images/menu_btn.svg',
-                            ),
-                          ),
-                          const SizedBox(width: 30),
-                          Container(
-                            padding: const EdgeInsets.all(15),
-                            width: 80,
-                            height: 80,
-                            decoration: BoxDecoration(
-                                color: const Color(0xFF035DA7),
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 3,
-                                ),
-                                borderRadius: BorderRadius.circular(100)),
-                            child: SvgPicture.asset(
-                              color: const Color(0xFFFFB159),
-                              'assets/images/restart_btn.svg',
-                            ),
-                          ),
-                          const SizedBox(width: 30),
-                          Container(
-                            padding: const EdgeInsets.all(15),
-                            width: 80,
-                            height: 80,
-                            decoration: BoxDecoration(
-                                color: const Color(0xFF035DA7),
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 3,
-                                ),
-                                borderRadius: BorderRadius.circular(100)),
-                            child: SvgPicture.asset(
-                              color: const Color(0xFFFFB159),
-                              'assets/images/play_btn.svg',
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+            Positioned(
+              top: -10,
+              right: -10,
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Color(0xFF035DA7),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 5,
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: SvgPicture.asset(
+                  color: Color(0xFFFFB159),
+                  'assets/images/close_btn.svg',
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
