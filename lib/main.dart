13ac7458d3c52a06baaf58b4976e1737067005ee
Mofolasayo Osikoya <osikoyamofolasayo@gmail.com>
2024-08-07 09:56:13 +0100

    landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
-import 'package:angry_birds/components/ground.dart';
-import 'package:angry_birds/components/obstacles.dart';
-import 'package:angry_birds/components/player.dart';
-import 'package:angry_birds/components/slingshot.dart';
-import 'package:angry_birds/levels/level1.dart';
-import 'package:angry_birds/levels/level2.dart';
-import 'package:angry_birds/levels/level3.dart';
-import 'package:angry_birds/levels/level4.dart';
-import 'package:angry_birds/levels/level5.dart';
 import 'package:angry_birds/levels/level6.dart';
-import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+  ]);
   runApp(GameWidget(
     game: Game(),
   ));
