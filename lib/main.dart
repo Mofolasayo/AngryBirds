c30b9740f1b225f55e55018ff37baa838d77b127
fidelity2851 <ukwuanifidelis2851@gmail.com>
2024-08-07 12:37:12 +0100

    Added some overlay screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:angry_birds/levels/level6.dart';
+import 'package:angry_birds/screens/overlays/game_won_overlay.dart';
+import 'package:angry_birds/screens/overlays/pause_overlay_screen.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
@@ -10,8 +12,18 @@ Future<void> main() async {
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeRight,
   ]);
-  runApp(GameWidget(
-    game: Game(),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: GameWidget(
+        game: Game(),
+        overlayBuilderMap: {
+          'PauseOverlay': (BuildContext context, Game game) {
+            return GameWonOverlay();
+          }
+        },
+      ),
+    ),
   ));
 }
 
@@ -23,8 +35,6 @@ class Game extends Forge2DGame with TapCallbacks {
     await super.onLoad();
     //add(Level1());
     add(Level6());
-
+    overlays.add('PauseOverlay');
   }
 }
-
-
---
+++ lib/screens/overlays/game_won_overlay.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class GameWonOverlay extends StatelessWidget {
+  const GameWonOverlay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.sizeOf(context).width / 2,
+              height: MediaQuery.sizeOf(context).height / 1.3,
+              padding: EdgeInsets.all(10),
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                color: Color(0xFF035DA7),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 5,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+              ),
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 80,
+                          child: SvgPicture.asset(
+                            color: Color(0xFFFFB159),
+                            'assets/images/fill_star.svg',
+                          ),
+                        ),
+                        SizedBox(width: 30),
+                        Container(
+                          width: 80,
+                          height: 80,
+                          child: SvgPicture.asset(
+                            width: 60,
+                            height: 60,
+                            color: Color(0xFFFFB159),
+                            'assets/images/fill_star.svg',
+                          ),
+                        ),
+                        SizedBox(width: 30),
+                        Container(
+                          width: 80,
+                          height: 80,
+                          child: SvgPicture.asset(
+                            width: 60,
+                            height: 60,
+                            color: Color(0xFFFFB159),
+                            'assets/images/open_star.svg',
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF047EE2),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          children: [
+                            Center(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'New Level Unlocked',
+                                    style: TextStyle(
+                                      color: Colors.amber,
+                                      height: 1,
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  Text(
+                                    'Current Score',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      height: 1,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    '10,000',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      height: 1,
+                                      fontSize: 50,
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Spacer(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(15),
+                                  width: 80,
+                                  height: 80,
+                                  decoration: BoxDecoration(
+                                      color: Color(0xFF035DA7),
+                                      border: Border.all(
+                                        color: Colors.white,
+                                        width: 3,
+                                      ),
+                                      borderRadius: BorderRadius.circular(100)),
+                                  child: SvgPicture.asset(
+                                    color: Color(0xFFFFB159),
+                                    'assets/images/menu_btn.svg',
+                                  ),
+                                ),
+                                SizedBox(width: 30),
+                                Container(
+                                  padding: EdgeInsets.all(15),
+                                  width: 80,
+                                  height: 80,
+                                  decoration: BoxDecoration(
+                                      color: Color(0xFF035DA7),
+                                      border: Border.all(
+                                        color: Colors.white,
+                                        width: 3,
+                                      ),
+                                      borderRadius: BorderRadius.circular(100)),
+                                  child: SvgPicture.asset(
+                                    color: Color(0xFFFFB159),
+                                    'assets/images/restart_btn.svg',
+                                  ),
+                                ),
+                                SizedBox(width: 30),
+                                Container(
+                                  padding: EdgeInsets.all(15),
+                                  width: 80,
+                                  height: 80,
+                                  decoration: BoxDecoration(
+                                      color: Color(0xFF035DA7),
+                                      border: Border.all(
+                                        color: Colors.white,
+                                        width: 3,
+                                      ),
+                                      borderRadius: BorderRadius.circular(100)),
+                                  child: SvgPicture.asset(
+                                    color: Color(0xFFFFB159),
+                                    'assets/images/next_btn.svg',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: -10,
+              right: -10,
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Color(0xFF035DA7),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 5,
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: SvgPicture.asset(
+                  color: Color(0xFFFFB159),
+                  'assets/images/close_btn.svg',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
